a1d65e5012658a0e6fd2bde96c1f3a0a9fbf5a2f
liujuntao <ljt6232310@163.com>
2023-09-11 10:07:29 +0800

    完成登陆登出

--- vue-element-admin-i18n/src/permission.js
+++ vue-element-admin-i18n/src/permission.js
@@ -17,7 +17,6 @@ router.beforeEach(async (to, from, next) => {
 
   // determine whether the user has logged in
   const hasToken = getToken()
-debugger
   if (hasToken) {
     if (to.path === '/login') {
       // if is logged in, redirect to the home page
